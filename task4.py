531b62ba90c1e6a3abbd2ca9cfd055214ece43f5
Lavanyaramavath <90607204+Lavanyaramavath@users.noreply.github.com>
2021-09-13 14:16:38 +0530

    Add files via upload
    13/09/2021

---
+++ task4.py
@@ -0,0 +1,6 @@
+a=input("Enter a string")#batch
+count=0
+for ch in a:
+    if(ch=='a'or ch=='e'or ch=='i'or ch=='o'or ch=='u'):
+        count+=1
+print("no of vowels in a given string are:",count)        
